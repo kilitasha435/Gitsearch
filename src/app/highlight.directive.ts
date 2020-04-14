77f6962032e95f8ae748c74e87972b51570491ac
kevin-kili <kilikevin435@gmail.com>
2020-04-14 10:35:41 +0300

    added the app highlight

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,29 +1,3 @@
-// import { Directive, ElementRef, HostListener, Input } from '@angular/core';
-
-// @Directive({
-//   selector: '[appHighlight]'
-// })
-// export class HighlightDirective {
-
-//   @Input('appHighlight') highlightColor:string;
-//   private el:HTMLElement;
-
-//   constructor(private el: ElementRef) {}
-
-//   @HostListener('mouseenter') onMouseEnter() {
-//     this.highlight('teal');
-//   }
-
-//   @HostListener('mouseleave') onMouseLeave() {
-//     this.highlight(null);
-//   }
-
-//   private highlight(color: string) {
-//     this.el.nativeElement.style.backgroundColor = color;
-//   }
-
-// }
-
 import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
@@ -40,7 +14,7 @@ this.el = el.nativeElement;
 }
 
 @HostListener('mouseenter') onMouseEnter() {
-this.highlight(this.highlightColor || 'lime');
+this.highlight(this.highlightColor || 'blue');
 }
 
 @HostListener('mouseleave') onMouseLeave() {
